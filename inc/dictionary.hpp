68f787cb5269b4b3fc72395ae3b50a941a5a0ba6
Deanamic <deanzhu1@gmail.com>
2016-10-31 16:11:18 +0100

    added throw error

--- inc/dictionary.hpp
+++ inc/dictionary.hpp
@@ -4,21 +4,27 @@
 #include <string>
 #include <set>
 #include <fstream>
+#include "error.hpp"
 
 class Dictionary {
     private:
-        std::set <std::string> Words;
+        std::set <std::string> mWords;
     public:
-        Dictionary( const std::string & fname ) {
-            std::ifstream wlist( fname.c_str() );
+        Dictionary(const std::string & fname) {
+            std::ifstream wlist(fname.c_str() );
+            if ( !wlist.is_open() ) {
+                throw ScheckError("Could not open dictionary file " + fname);
+            }
             std::string word;
-            while( std::getline( wlist, word ) ) {
-                Words.insert( word );
+            while ( std::getline(wlist, word) )
+                mWords.insert(word);
+            if ( !wlist.eof() ) {
+                throw ScheckError("Error reading dictionary file " + fname);
             }
         }
-        bool Check( const std::string & word ) const {
-            return Words.find( word ) != Words.end();;
+        bool Check(const std::string & word) const {
+            return mWords.find(word) != mWords.end();;
         }
 };
 
-#endif
+#endif // ifndef INC_DICTIONARY_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <string>
 #include "dictionary.hpp"
+#include "error.hpp"
 using namespace std;
 
 int main() {
     cout << "scheck version 0.1" << endl;
-    Dictionary d( "data/mydict.dat" );
+    Dictionary d( "data/not-there-mydict.dat" );
     string word;
-    cin >> word;
+    string line;
+    while( getline( cin, line ) )  {
+        //parse( line );
+    }
     if ( d.Check( word ) ) {
         cout << word << " is OK\n";
     }
